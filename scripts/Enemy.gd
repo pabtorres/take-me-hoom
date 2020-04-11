649f2b18094c6e6a25c36d3088a42e02865842a5
adelebrgx <adele.bourgeix@gmail.com>
2020-04-11 19:49:30 -0400

    added fox enemy with animations and attack

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,38 +1,62 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Para que ataque cada 100 frames
+var time
+
+# Para definir los puntos de vida del enemigo 
 var alive
 var lifePoints
-export var velocity = Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	time=0
 	alive=true;
 	lifePoints=50;
-	print(lifePoints)
-	#move_and_slide(velocity, Vector2.UP, true)
-	pass # Replace with function body.
+	
+
+func set_attack(value: bool):
+	if alive==true:
+		$Attack.disabled=!value
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+func _process(delta):
+	if time!=100 && $AnimationPlayer.current_animation != "Attack" && $AnimationPlayer.current_animation != "Die":
+		$AnimationPlayer.play("Idle")
+		time+=1
+	if time==100:
+		$AnimationPlayer.play("Attack")
+		time=0
+	 
+		
+		
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name=="Die":
+		dead()
 
 func _on_Enemy_body_entered(body):
-	print("hello")
+	#print(body.get_name())
+	#print("hello")
 	if alive:
 		lifePoints-=10;
 		if lifePoints<=0:
 			alive=false
-			$icon.visible = not $icon.visible
+			print("dead")
+			$AnimationPlayer.play("Die")
+			#$Attack.disabled=true
+			#$CollisionShape2D.disabled=true
+			#$icon.visible = not $icon.visible
 			
 			
 		
 	print(lifePoints)
-	print(alive)
-	print(body)
+	#print(alive)
+	#print(body)
+
+func dead():
+	$Attack.disabled=true
+	$CollisionShape2D.disabled=true
+	$icon.visible = not $icon.visible
+
