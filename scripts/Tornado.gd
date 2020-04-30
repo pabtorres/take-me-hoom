1bcf6390663b818550209e8fdf389a07c7bb9661
adelebrgx <adele.bourgeix@gmail.com>
2020-04-30 16:37:27 -0400

    owl throws tornados

--- scripts/Tornado.gd
+++ scripts/Tornado.gd
@@ -1,17 +1,32 @@
 extends Area2D
 
+var dir="left"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var destroyed = false
+var speed = 130
+
+var velocity = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if dir=="right":
+		velocity = speed * (0.8*transform.x+0.2*transform.y)
+	else:
+		velocity = speed * (-0.8*transform.x+0.2*transform.y)
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$AnimationPlayer.play("Blow")
+	position += velocity * delta
+	
 
+
+
+
+
+func _on_Tornado_body_entered(body):
+	$Sprite.visible=false
--- scripts/owl.gd
+++ scripts/owl.gd
@@ -1,22 +1,46 @@
 extends Area2D
 
 var time=0
-
+var velocity
+const bullet_obj = preload("res://scenes/Tornado.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	
 	if time==100:
 		if $Sprite.flip_h==false:
 			$Sprite.flip_h=true
+			var bullet_inst = bullet_obj.instance()
+			print("instantiated")
+			bullet_inst.dir="right"
+			bullet_inst.position.x=0
+			bullet_inst.position.y=-10
+			print(bullet_inst.global_position)
+			bullet_inst.rotation = rotation
+			$"../Bullets".add_child(bullet_inst)
+			
 		else:
 			$Sprite.flip_h=false
+			var bullet_inst = bullet_obj.instance()
+			print("instantiated")
+			bullet_inst.dir="left"
+			bullet_inst.position.x=0
+			bullet_inst.position.y=-10
+			velocity=bullet_inst.speed * (0.8*transform.x+0.2*transform.y)
+			velocity*=-1
+			bullet_inst.velocity=velocity
+			print(bullet_inst.global_position)
+			bullet_inst.rotation = rotation
+			$"../Bullets".add_child(bullet_inst)
 		time=0
 	time+=1
+	
 	$AnimationPlayer.play("Idle")
 
