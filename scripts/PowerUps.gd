ed57933ae95850e03231e568d472ead3d0cd9357
Tomás Estévez Lenz <tomas.estevez@ug.uchile.cl>
2020-06-04 22:59:17 -0400

    little powerup animation added

--- scripts/PowerUps.gd
+++ scripts/PowerUps.gd
@@ -3,14 +3,22 @@ extends Area2D
 
 # Declare member variables here. Examples:
 # var a = 2
-# var b = "text"
-
+var v0 = -0.1
+var bars = 0
+var speed = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+func _physics_process(delta):
+	var move = Vector2()
+	if bars >= 30:
+		speed = -speed
+		bars = 0
+	bars += 1
+	move.y = speed
+	position += move * delta
 
 func _on_JumpPowerUp_body_entered(body):
 	queue_free()
