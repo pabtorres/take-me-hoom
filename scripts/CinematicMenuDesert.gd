6d3822176a0cd0e3e0e307511fe76acc3a55e576
Tomás Estévez Lenz <tomas.estevez@ug.uchile.cl>
2020-06-07 16:31:51 -0400

    code cleaning

--- scripts/CinematicMenuDesert.gd
+++ scripts/CinematicMenuDesert.gd
@@ -1,19 +1,9 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _on_Button_pressed():
-	#LevelManager.turn_to_day()
-	#get_tree().change_scene("res://scenes/DesertDay.tscn")
-	#MusicManager.change_desert_day_music()
 	LevelManager.change_level_to_desert()
-	#MusicManager.change_music(1)
 	LevelManager.reset_life_points()
